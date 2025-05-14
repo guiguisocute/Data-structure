0b5a6d88c70f069e6b47804b03f446d7f0091ce2
guiguisocute_lap <g244629079@gmail.com>
2025-05-14 23:16:49 +0800

    small change

--- lab/Experiment7-Binary Tree Implementation/lab7_07.c
+++ lab/Experiment7-Binary Tree Implementation/lab7_07.c
@@ -3,11 +3,37 @@
 */
 
 #include "bintree.h"
-char *a="ABC##D#E##F##";  			/*扩充二叉树序树t的前序序列*/
+char *a = "ABCDE#####F##"; /*扩充二叉树序树t的前序序列*/
+static bintree parents[N];
+static bintree nodes[N];
+static int count = 0;
+
+void buildparents(bintree t,bintree p){     //p传参主要用于递归，表达逻辑父
+    if(!t)
+        return;
+    if(count < N){
+        nodes[count] = t;
+        parents[count] = p;
+        count++;
+    }
+    buildparents(t->lchild,t);
+    buildparents(t->rchild,t);
+}
+
+bintree searchparent(bintree t){
+    for(int i = 0; i < count; i++){
+        if( nodes[i] == t)
+            return parents[i];
+    }
+    return NULL;
+}
+
 /*
     函数名称：findClosest(t,p)
     函数功能：从二叉树t中，查找距离*p最近的叶子结点
 */
+
+
 bintree  findClosest(bintree t,bintree p)//请将本函数补充完整
 /*其实这道题我还是挺不理解的，这道题到底是要我找这颗子树的最近叶节点，还是包括向“上”检索的包括兄弟树叶节点？
 姑且先当做后者来解吧，毕竟后者算是前者的一个子集
@@ -18,16 +44,56 @@ bintree  findClosest(bintree t,bintree p)//请将本函数补充完整
 while(队非空){
     判队首为叶否？
         是的话return
-    队首父入队 tag = 1
     队首左孩，右孩入队，tag = 1
+    队首父入队 tag = 1
+    （这个tag是防止走父→子→父的死循环）
+    出队
 }
     return null?
 
-
+思路解决完现在要思考怎么获取父节点
+最蠢的方法肯定是每次需要用父节点的时候都遍历一遍，这个绝对不行时间复杂度爆炸
+一个方法是把树边改成一个双向的指针，改成类似双向链表这种形式，但是要怎么实现？
 */
 
 {
+    //先dfs一遍添加父节点，好像只能再新建一个新函数要不然没法递归啊，如果不动头文件，可以用全局数组建立映射，然后再用一个searchparent函数一一去找就行
+    bintree quene[N];
+    int visited[N] = {0};
+    int r = 0, f = 0;
+    quene[r] = p;
+    r++;    //初始化队列
     
+    for(int i = 0; i < count; i++){
+        if(nodes[i] == p){
+            visited[i] = 1;
+            break;
+        }
+
+    }//初始访问上标记
+    
+    while(f < r){
+        bintree u = quene[f++];
+        if(!u->lchild && !u->rchild)
+            return u;
+        bintree next[3] = {u->lchild,u->rchild,searchparent(u)};
+        for(int k = 0; k < 3 ; k++){
+            bintree v = next[k];
+            if(!v)
+                continue;   //跳过空子树或空父（这种说法好好笑）
+            
+        for(int i = 0; i < count; i++){
+            if(nodes[i] == v && visited[i] != 1){
+                quene[r++] = v;
+                visited[i] = 1;
+                break;
+                }
+            }
+        }
+    }
+
+
+    return NULL;
 }
 
 /*
@@ -51,8 +117,10 @@ int main()
     bintree t,p,q;
     char c;
     t=creatbintree();   	/*建立二叉树t的存储结构*/
+    buildparents(t,NULL);
     printf("二叉树的前序序列为：\n");
     preorder(t);
+
     printf("\n请输入一个结点值:");
     scanf("%c",&c);
     p=search(t,c);
