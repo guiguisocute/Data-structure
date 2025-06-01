cd6b77535838825f0814c6e3823c45d33d6b3821
guiguisocute_lap <g244629079@gmail.com>
2025-06-01 19:57:14 +0800

    收工

--- lab/Experiment10-Sorting Techniques/lab10_05.c
+++ lab/Experiment10-Sorting Techniques/lab10_05.c
@@ -4,10 +4,18 @@
 对a[1]..a[n]进行升序排序。并测试在不同数据规模下的排序效率。（详见lab10_05.c）
 */
 #include "Arrayio.h"
-#define N 10000     /*N为数据量大小，因data1.txt中只有50万个数，所以自行设定N值时需让N<=500000*/
+#include <time.h>
+#define N 500000     /*N为数据量大小，因data1.txt中只有50万个数，所以自行设定N值时需让N<=500000*/
 
 /*请将本函数补充完整，并进行测试*/
-void sift(int a[],int k,int n)
+//堆在现阶段可以简单理解成一个完全二叉树，除最后一层其他必须满，最后一层即使不满也必须要是连续的
+//大根堆：任一节点都大于等于其左右孩子，小根堆就是反过来
+//所以你明白了吧，既然完全二叉树，那么就可以用顺序结构从上到下从左到右存进数组里，不会造成空间的浪费：
+//左孩子：2i，右孩子：2i+1，找父母就是除2向下取整 **这个是从下标1开始存的规律**
+//本质利用堆顶是最大值or最小值去排序
+//具体的实现是把一个数组看成一个逻辑上的完全二叉树，然后把这颗二叉树给大根堆化，最后输出的时候把根与最后的节点一一交换就可以了
+//关键是要理解怎么堆化
+void sift(int a[], int k, int n) // 参数：k = 需要调整的根下标；n = 当前堆最后一个元素下标。作用是在确保下面的节点已经堆化的情况，对于该处可能破坏堆结构的根节点，往下放，直到这颗完全二叉树能够大根堆化
 {
       int i,j,finished;
       i=k;j=2*i;
@@ -32,13 +40,13 @@ void heapSort(int a[],int n)
 {
     int i;
     for (i=n/2;i>=1;i--)
-        sift(a,i,n);
+        sift(a,i,n);        //目的是把数组给堆化
     for (i=n;i>1;i--)
         {
             a[0]=a[i];
             a[i]=a[1];
             a[1]=a[0];
-            sift(a,1,i-1);
+            sift(a,1,i-1);  //交换完成以后，把刚放下去的max从堆中去掉（i-1），然后再向下调整新上根的那个结点，把新的完全二叉树给堆化
         }
 }
 
@@ -48,8 +56,12 @@ int  main()
   printf("数据初始化...\n");
   n=readData(a,N,"data1.txt");      /*从data1.txt中读入N个整数存入数组a，n为实际读入的数据个数*/
   printf("%d个数据排序中...\n",n);
-  heapSort(a,n);
-  saveData(a,n,"out.txt");          /*排序结果存放在out.txt文件中*/
+  clock_t beg = clock();
+  heapSort(a, n);
+  clock_t end = clock();
+  saveData(a, n, "out.txt"); /*排序结果存放在out.txt文件中*/
   printf("排序结束，排序结果保存在out.txt文件中。\n");
+  printf("n=%d,  time = %.3f ms\n",
+         n, 1000.0 * (end - beg) / CLOCKS_PER_SEC);
   return 0;
 }
